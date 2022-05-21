5bec602c9e89e1dd504d50ad0b6e2008e7a2d9d5
Teslagirl69 <teslagirl17@gmail.com>
2022-05-22 02:18:25 +0300

    Fix barber[name]

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'activerecord', '~> 6.0'
 gem "sinatra-activerecord"
 gem "sinatra-contrib"
 gem "rake"
-
+gem 'pry', '~> 0.13.1'
 group :development do
 
   gem "tux"
--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'pry'
 
 
 #устанавливаем и создаем БД
@@ -18,7 +19,35 @@ end
 class Barber  < ActiveRecord::Base
 end
 
+
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
 	@barbers = Barber.all
 	erb :index
+end
+
+
+get '/visit' do
+
+ @user = Client.new
+	erb :visit
+end
+
+colors = { "#7bd148" => 'Зеленый', "#5484ed" => 'Темно-Голубой', "#a4bdfc" => 'Голубой', "#46d6db" => 'Бирюзовый',
+"#7ae7bf" => 'Светло-Зеленый', "#51b749" => 'Темно-Зеленый', "#fbd75b" => 'Желтый', "#ffb878" => 'Оранжевый', "#ff887c" => 'Красный',
+"#dc2127" => 'Темно-Красный', "#dbadff" => 'Пурпурный', "#e1e1e1" => 'Серый'}
+
+
+post '/visit' do
+
+
+	@user = Client.new params[:client]
+
+ 	@user.save
+
+	erb "<h2> пасибо, запись прошла успешно </h2>"
+
 end
